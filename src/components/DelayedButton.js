dfc6c815e99ed9a8fa78f367bb98f1f2b7b574e9
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-22 11:13:44 +0000

    automatically backed up by learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,8 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react'
+export default class DelayedButton extends Component {
+  constructor(funct, delaynumber){
+    onDelayedClick: funct, 
+    delay: delaynumber
+  }
+}
